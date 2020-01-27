b17bfa17978b2851f5957dddf042129953273d32
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-27 13:48:06 +0000

    removing unused imported module

--- http-request.ts
+++ http-request.ts
@@ -1,20 +1,58 @@
 import * as https from 'https'
+// import * as request from 'request'
 
-export function httpGet(url: string, cookie: string = ''): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
+type GetResponse = string | { content, cookie }
+
+export function httpGet(url: string, cookie: string = '', returnCookie: boolean = false): Promise<GetResponse> {
+    return new Promise<GetResponse>((resolve, reject) => {
         https.get(url, { headers: {'cookie': cookie}}, response => {
             let data = ''
         
             response.on('data', chunk => {
                 data += chunk
             })
+
+            
         
             response.on('end', () => {
-                resolve(data)
+                returnCookie ? resolve({
+                    content: data,
+                    cookie: JSON.stringify(response.headers, null, 4)
+                }) : resolve(data)
             })
         }).on('error', err => {
             console.log('error retrieving request')
             reject(`Error: ${err.message}`)
         })
+    })
+}
+
+export function httpPost(url: {
+    host: string, 
+    path: string, 
+    headers: { 
+        contentType: string, 
+        cookie: string }
+    }, 
+    data: any): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+        const options = {
+            hostname: url.host,
+            port: 443,
+            path: url.path,
+            method: 'POST',
+            headers: {
+              'Cookie': url.headers.cookie
+            }
+          }
+
+        const req = https.request(options, (response) => {
+            response.on('data', d => resolve((d as Buffer).toString()))
+        });
+        
+        req.on('error', error => reject(error))
+        req.write(data)
+        req.end()
+
     })
 }
