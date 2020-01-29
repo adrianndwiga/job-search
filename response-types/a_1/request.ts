2170efa5f4897fd0053f3ee1feb61fa1fd1a4765
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-29 12:43:27 +0000

    implementing job search request

--- response-types/a_1/request.ts
+++ response-types/a_1/request.ts
@@ -5,14 +5,14 @@ import * as https from 'https'
 import { HttpsRequest } from "../types";
 import { unzip } from "zlib";
 
-export type LoadRequestHeaders = (header: string)  => {}
+export type LoadRequestHeaders = (header: string) => {}
 export type GetAuthResponseFromBody = (content: string) => AuthResponse
 
 export class AuthRequest implements HttpsRequest<AuthRequestConfig, AuthResponse> {
     request(options: AuthRequestConfig): Promise<AuthResponse> {
         return new Promise<AuthResponse>(async (resolve, reject) => {
             const response = await baseRequest({
-                host: options.host, 
+                host: options.host,
                 path: options.path,
                 method: options.method,
                 headers: this.loadHeaders(options.headers)
@@ -40,7 +40,7 @@ export class AuthRequest implements HttpsRequest<AuthRequestConfig, AuthResponse
 export class JobSearchRequest implements HttpsRequest<JobSearchRequestConfig, JobSearchResponse> {
 
     request(options: JobSearchRequestConfig): Promise<JobSearchResponse> {
-        return new Promise<JobSearchResponse>( async (resolve, reject) => {
+        return new Promise<JobSearchResponse>(async (resolve, reject) => {
             const requestOptions: https.RequestOptions = {
                 host: options.host,
                 path: options.path,
@@ -48,53 +48,27 @@ export class JobSearchRequest implements HttpsRequest<JobSearchRequestConfig, Jo
                 method: options.method,
                 headers: options.headers
             }
-
-            let counter: number = 1
-            let i: JobSearchResponse = {page: counter, pageCount: counter}
-            while (i.page <= i.pageCount) {
-                const response = await baseRequest(requestOptions, {
-                    page: 1,
-                    shid: this.authResponse.shid
-                })
-                let chunk = ''
-                response.on('data', data => chunk += data)
-                response.on('end', () => {
-                    // resolve({
-                    //     page: 1,
-                    //     pageCount: 1
-                    // })
-                    const responseData = JSON.parse(chunk) as {page: number, pageCount: number}
-                    counter += 1
-                    i.page = counter
-                    i.pageCount = responseData.pageCount
-                })
-            }
-
-            // baseRequest(requestOptions, {
-            //     page: 1,
-            //     shid: this.authResponse.shid
-            // })
-            // .then(response => {
-            //     let chunk = ''
-            //     response.on('data', data => chunk += data)
-            //     response.on('end', () => {
-            //         resolve({
-            //             page: 1,
-            //             pageCount: 1
-            //         })
-            //     })
-            // })
+            const response = await baseRequest(requestOptions, JSON.stringify({
+                page: 1,
+                shid: this.authResponse.shid
+            }))
+            let chunk = ''
+            response.on('data', data => chunk += data)
+            response.on('end', () => {
+                const responseData = JSON.parse(chunk) as { page: number, pageCount: number }
+                resolve(responseData)
+            })
         })
     }
 
     constructor(
-        private config: JobSearchRequestConfig, 
-        private authResponse: AuthResponse, 
-        private jobSearchResponse: JobSearchResponse) {  
+        private config: JobSearchRequestConfig,
+        private authResponse: AuthResponse,
+        private jobSearchResponse: JobSearchResponse) {
     }
 }
 
-export const baseRequest = (options: https.RequestOptions, data = undefined) : Promise<IncomingMessage> => {
+export const baseRequest = (options: https.RequestOptions, data = undefined): Promise<IncomingMessage> => {
     return new Promise<IncomingMessage>((resolve, reject) => {
         const request = https.request(options, response => {
             resolve(response)
