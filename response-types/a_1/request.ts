9f5a272f93b6754c67f2345b4de1a4bb7f5988ae
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-29 11:46:22 +0000

    fixing handling of request

--- response-types/a_1/request.ts
+++ response-types/a_1/request.ts
@@ -1,19 +1,46 @@
 import { AuthRequestConfig, JobSearchRequestConfig } from "./request-config";
 import { AuthResponse, JobSearchResponse } from "./response";
-import { IncomingMessage, OutgoingHttpHeaders } from "http";
+import { IncomingMessage } from "http";
 import * as https from 'https'
 import { HttpsRequest } from "../types";
-import { resolve } from "dns";
+import { unzip } from "zlib";
 
-export class AuthRequest {
-    constructor(config: AuthRequestConfig) {
+export type LoadRequestHeaders = (header: string)  => {}
+export type GetAuthResponseFromBody = (content: string) => AuthResponse
+
+export class AuthRequest implements HttpsRequest<AuthRequestConfig, AuthResponse> {
+    request(options: AuthRequestConfig): Promise<AuthResponse> {
+        return new Promise<AuthResponse>(async (resolve, reject) => {
+            const response = await baseRequest({
+                host: options.host, 
+                path: options.path,
+                method: options.method,
+                headers: this.loadHeaders(options.headers)
+            }, this.requestData)
+
+            let returnedResponse: Buffer
+
+            response.on('data', data => {
+                returnedResponse = data
+            })
+
+            response.on('end', () => {
+                unzip(returnedResponse, (err, buffer) => {
+                    resolve(this.getAuthResponseFromBody(buffer.toString()))
+                })
+            })
+
+            response.on('error', error => reject(error))
+        })
+    }
+    constructor(private readonly loadHeaders: LoadRequestHeaders, private readonly requestData: string, private readonly getAuthResponseFromBody: GetAuthResponseFromBody) {
     }
 }
 
-export class JobSearchRequest implements HttpsRequest<JobSearchRequestConfig, {page: number, shid: string}> {
+export class JobSearchRequest implements HttpsRequest<JobSearchRequestConfig, JobSearchResponse> {
 
-    request(options: JobSearchRequestConfig): Promise<{page: number, shid: string}> {
-        return new Promise<{page: number, shid: string}>((resolve, reject) => {
+    request(options: JobSearchRequestConfig): Promise<JobSearchResponse> {
+        return new Promise<JobSearchResponse>( async (resolve, reject) => {
             const requestOptions: https.RequestOptions = {
                 host: options.host,
                 path: options.path,
@@ -21,17 +48,42 @@ export class JobSearchRequest implements HttpsRequest<JobSearchRequestConfig, {p
                 method: options.method,
                 headers: options.headers
             }
-            baseRequest(requestOptions)
-            .then(response => {
+
+            let counter: number = 1
+            let i: JobSearchResponse = {page: counter, pageCount: counter}
+            while (i.page <= i.pageCount) {
+                const response = await baseRequest(requestOptions, {
+                    page: 1,
+                    shid: this.authResponse.shid
+                })
                 let chunk = ''
                 response.on('data', data => chunk += data)
                 response.on('end', () => {
-                    resolve({
-                        page: 1,
-                        shid: this.authResponse.shid
-                    })
+                    // resolve({
+                    //     page: 1,
+                    //     pageCount: 1
+                    // })
+                    const responseData = JSON.parse(chunk) as {page: number, pageCount: number}
+                    counter += 1
+                    i.page = counter
+                    i.pageCount = responseData.pageCount
                 })
-            })
+            }
+
+            // baseRequest(requestOptions, {
+            //     page: 1,
+            //     shid: this.authResponse.shid
+            // })
+            // .then(response => {
+            //     let chunk = ''
+            //     response.on('data', data => chunk += data)
+            //     response.on('end', () => {
+            //         resolve({
+            //             page: 1,
+            //             pageCount: 1
+            //         })
+            //     })
+            // })
         })
     }
 
