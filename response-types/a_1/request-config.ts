8bd451d664f76a9d56fbe9731e3890740872a8c9
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-30 15:14:09 +0000

    removing redundant comments code

--- response-types/a_1/request-config.ts
+++ response-types/a_1/request-config.ts
@@ -1,5 +1,3 @@
-import { readFileSync } from "fs"
-
 interface RequestConfig {
     host: string
     path: string
@@ -7,17 +5,7 @@ interface RequestConfig {
 }
 
 export interface AuthRequestConfig extends RequestConfig {
-//     constructor(headers: string) {
-//         const headerFields = readFileSync(headers, 'utf8')
-//         for(const field of headerFields.split('\n')) {
-//             const values = field.split(': ')
-//             this.headers[values[0]] = values[1]
-//         }
-//     }
-    // host: string
-    // path: string
     headers: string
-    // method: "POST"
 }
 
 export interface JobSearchRequestConfig extends RequestConfig {
