07789ff1116e87ce08c9fd08672d8c7547e4384a
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-01-31 17:49:32 +0000

    fixing linting errors

--- request.ts
+++ request.ts
@@ -1,25 +1,6 @@
 import * as https from 'https'
 import { IncomingMessage } from 'http'
 
-export function httpGet(url: string, cookie: string = ''): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        https.get(url, { headers: {'cookie': cookie}}, response => {
-            let data = ''
-        
-            response.on('data', chunk => {
-                data += chunk
-            })
-        
-            response.on('end', () => {
-                resolve(data)
-            })
-        }).on('error', err => {
-            console.log('error retrieving request')
-            reject(`Error: ${err.message}`)
-        })
-    })
-}
-
 export const baseRequest = (options: https.RequestOptions, data = ''): Promise<IncomingMessage> => {
     return new Promise<IncomingMessage>((resolve, reject) => {
         const request = https.request(options, response => {
