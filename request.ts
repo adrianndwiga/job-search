a29eaf7029131b7b098dbae81751b38d176de6ec
Adrian Ndwiga <adrian_ndwiga@hotmail.com>
2020-02-17 16:32:39 +0000

    fixing linting errors on max length

--- request.ts
+++ request.ts
@@ -1,7 +1,10 @@
 import * as https from 'https'
 import { IncomingMessage } from 'http'
 
-export const baseRequest = (options: https.RequestOptions, data = ''): Promise<IncomingMessage> => {
+export const baseRequest = (
+    options: https.RequestOptions,
+    data = ''): Promise<IncomingMessage> => {
+
     return new Promise<IncomingMessage>((resolve, reject) => {
         const request = https.request(options, response => {
             resolve(response)
